4a215024c6b29093050e07653cee97b2fd8d0cc4
Muhabullah Rifat <51780745+Rifat01@users.noreply.github.com>
2020-01-15 01:59:05 +0600

    Add files via upload

---
+++ css/contact.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function(){
+  $("#contact123").mouseenter(function(){
+    $("#contact1").show("slow");
+  });
+    $("#contact123").mouseleave(function(){
+    $("#contact1").hide("slow");
+  });
+});*/
